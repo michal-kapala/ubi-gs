5fb4cbea2f360bbbd1a5482c4bcc66662088c23f
michal-kapala <mim4k1@gmail.com>
2024-08-05 19:01:26 +0200

    add router stub

--- homm5/homm5.bat
+++ homm5/homm5.bat
@@ -5,4 +5,7 @@ echo.
 echo Starting IRC...
 start "Heroes of Might and Magic V - IRC" python irc.py
 echo.
+echo Starting Router...
+start "Heroes of Might and Magic V - Router" python router.py
+echo.
 exit
